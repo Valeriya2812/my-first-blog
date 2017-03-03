feae1ce21423a8bb2bd0dc0a5e5b60b60e833380
Valeriia <valerychernikova@gmail.com>
2017-03-03 22:13:50 +0200

    add image2

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@
 #from django.conf.urls.staticfiles import staticfiles
 from . import views
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
